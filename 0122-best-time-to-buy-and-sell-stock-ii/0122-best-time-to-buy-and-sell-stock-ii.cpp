822cce6f16d66e77d02d435136a6dbe68afc6744
Piraveen Raja N K <139096725+Piraveen12@users.noreply.github.com>
2024-08-30 06:06:19 +0530

    Time: 4 ms (71.63%), Space: 16 MB (43.59%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int ans = 0;
+        int i=1;
+        int prev=prices[0];
+        while(i<prices.size()){
+            if(prices[i]>prev){
+                while(i<prices.size()&&prices[i-1]<prices[i]){
+                    i++;
+                }
+            }
+            ans+=max(0,prices[i-1]-prev);
+            prev=prices[i];
+            i++;
+        }
+        return ans;
+    }
+};
