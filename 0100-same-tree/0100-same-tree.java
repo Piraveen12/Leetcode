c1acf78d04412eb331f7231f747a5efa0064db68
Piraveen Raja N K <139096725+Piraveen12@users.noreply.github.com>
2025-01-27 12:14:37 +0530

    Time: 0 ms (100%), Space: 40.9 MB (92.27%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,17 +14,25 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null &&q==null){
+    public boolean sametree(TreeNode p, TreeNode q){
+        if(p== null && q== null){
             return true;
         }
-        if(p==null||q==null){
+        if(p == null && q!=null){
+            return false;
+        }
+        if(p!=null && q== null){
             return false;
         }
         if(p.val!=q.val){
             return false;
         }
-        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
-        
+         
+        return sametree(p.left,q.left) && sametree(p.right,q.right);
+       
+
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return sametree(p,q);
     }
 }
