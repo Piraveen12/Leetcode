c7feab5fc7d5bb4eea74eb181b40125edb41621e
Piraveen Raja N K <139096725+Piraveen12@users.noreply.github.com>
2025-02-19 07:09:58 +0530

    Time: 30 ms (68.01%), Space: 52.5 MB (25%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       Arrays.sort(nums);
-        int n=nums.length-1;
-        List<List<Integer>>result=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(i!=0 && nums[i]==nums[i-1])
-                continue;
-           int left=i+1;
-           int right=n;
-           while(left<right){
-            int sum=nums[left]+nums[right]+nums[i];
-            if(sum==0){
-                List<Integer>ans =new ArrayList<>();
-                ans.add(nums[i]);
-                ans.add(nums[left]);
-                ans.add(nums[right]);
-                result.add(new ArrayList(ans));
-                left++;
-                right--;
-                while(left<right && nums[left]==nums[left-1] ){
-                 left++;
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int len = nums.length;
+        for(int i=0;i<len ;i++){
+             if(i> 0 && nums[i]==nums[i-1]){
+                    continue;
+                 }
+            int val = nums[i];
+            int left = i+1;
+            int right = len -1;
+            while(left < right){
+                int sum=val+nums[left]+nums[right];
+                if( sum==0){
+                    List<Integer> ans = new ArrayList<>();
+                    ans.add(nums[i]);
+                    ans.add(nums[left]);
+                    ans.add(nums[right]);
+                    result.add(ans);
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    right--;
+                    left++;
                 }
-                while(left<right && nums[right]==nums[right+1]){
+               
+               else  if(sum>0 ){
                     right--;
                 }
+                else{
+                    left ++;
+                }
             }
-            else if(sum<0){
-                left++;
-            }
-            else{
-                right--;
-            }
-           }
-        } 
+        }
         return result;
     }
 }
