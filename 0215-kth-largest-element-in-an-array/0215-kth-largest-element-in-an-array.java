2d90f4ddc1cecde70ab35e1238733567904cef4a
Piraveen Raja N K <139096725+Piraveen12@users.noreply.github.com>
2025-03-18 11:29:40 +0530

    Time: 40 ms (70.62%), Space: 55.7 MB (97.6%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,9 +1,41 @@
 class Solution {
+    private void merger(int le ,int mid  ,int ri ,int [] nums){
+        int[] left = new int[mid - le + 1]; 
+        int[] right = new int[ri - mid]; 
+
+        for (int i = 0; i < left.length; i++) { 
+            left[i] = nums[le + i]; 
+        }
+        for (int i = 0; i < right.length; i++) { 
+            right[i] = nums[mid + 1 + i]; 
+        }
+
+        int lst = 0, rst = 0, index = le;
+        while (lst < left.length && rst < right.length) { 
+            if (left[lst] <= right[rst]) { 
+                nums[index++] = left[lst++]; 
+            } else { 
+                nums[index++] = right[rst++]; 
+            }
+        }
+    
+        while (lst < left.length) { 
+            nums[index++] = left[lst++]; 
+        }
+        while (rst < right.length) { 
+            nums[index++] = right[rst++]; 
+        }
+    }
+    private void merge(int st,int end,int[] nums){
+        if(st< end){
+            int mid = (st+end)/2;
+            merge(st,mid,nums);
+            merge(mid+1,end,nums);
+            merger(st,mid,end,nums);
+        }
+    }
     public int findKthLargest(int[] nums, int k) {
-     Arrays.sort(nums);
-     if(k > nums.length){
-        return -1;
-     }
+    merge(0,nums.length-1,nums);
      return nums[nums.length-k];   
     }
 }
