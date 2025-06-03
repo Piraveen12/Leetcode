0ef3b3cb5a21aa23b013ee46b5f3b0f266268ef3
Piraveen Raja N K <139096725+Piraveen12@users.noreply.github.com>
2025-06-03 14:57:16 +0530

    Time: 26 ms (17.56%), Space: 94.6 MB (59.41%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,19 +1,18 @@
 class Solution {
-    public void recursion(int st,int n ,int k,List<List<Integer>>ans,List<Integer> result){
+    public void com(List<List<Integer>> ans,List<Integer> result, int st ,int k ,int n){
         if(result.size()== k){
             ans.add(new ArrayList(result));
         }
-        for(int i=st ; i <= n; i++ ){
+        for(int i =st ;i <= n ;i++){
             result.add(i);
-            recursion(i+1,n,k,ans,result);
+            com(ans,result,i+1,k,n);
             result.remove(result.size()-1);
         }
     }
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>>ans = new ArrayList<>();
-        List <Integer>result= new ArrayList<>();
-        int st=1;
-        recursion(st,n,k,ans,result);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        com(ans,result, 1,k,n);
         return ans;
     }
 }
