7da604f7de2d58650d32fbacc0869201a0f4c10e
Piraveen Raja N K <139096725+Piraveen12@users.noreply.github.com>
2025-06-03 16:01:21 +0530

    Time: 3 ms (31.92%), Space: 45.3 MB (13.78%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,40 +1,44 @@
 class Solution {
-    int [][]direction = new int [][] {{1,0},{0,1},{-1,0},{0,-1}};
+    int[][] directions = new int[][]{{1,0},{-1,0},{0,-1},{0,1}};
     public int maxAreaOfIsland(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        boolean visited[][]=new boolean [row][col];
-        int countiland = 0;
-        for(int st =0 ;st < row; st++){
-            for(int en =0 ; en < col ; en++){
-                if(grid[st][en]==0 || visited[st][en]){
-                    continue;
-                }
-                int count =1;
-                Queue<int[]> q = new LinkedList<>();
-                q.add(new int[]{st,en});
-                visited[st][en ]=true;
-                while(!q.isEmpty()){
-                    int []result = q.poll();
-                    int ro= result[0];
-                    int co= result[1];
-                    for(int[]node :direction ){
-                        int nodest= ro+node[0];
-                        int colust= co+node[1];
-                        if(nodest< 0 || nodest >=row || colust <0|| colust >=col){
+    int n = grid.length;
+    int max = 0;
+    
+    int m = grid[0].length;
+    boolean [][] arr = new boolean[n][m];
+    Queue<int[]> que = new LinkedList<>();
+    for(int i =0;i<n;i++){
+        for(int j =0 ;j<m;j++){
+            if(grid[i][j] == 0 || arr[i][j]){
+                continue;
+            }
+            if(grid[i][j] ==  1){
+                que.add(new int[]{i,j});
+                int sum =1;
+                arr[i][j] =true;
+                while(!que.isEmpty()){
+                    int[] val = que.poll();
+                    int ro = val[0];
+                    int co = val[1];
+                    for(int[] dir : directions){
+                        int row = ro + dir[0];
+                        int col = co + dir[1];
+                        if(row >= n || col >= m || row <0 || col <0){
                             continue;
                         }
-                        if(grid[nodest][colust]==0||visited[nodest][colust]){
+                        if(arr[row][col]|| grid[row][col] == 0 ){
                             continue;
                         }
-                        count++;
-                        q.add(new int[]{nodest,colust});
-                        visited[nodest][colust]=true;
+                        sum++;
+                        que.add(new int[]{row,col});
+                        arr[row][col] = true;
                     }
+                    max= Math.max(sum,max);
                 }
-                countiland= Math.max(countiland, count);
             }
         }
-        return countiland;
+      
+    }  
+    return max;
     }
 }
